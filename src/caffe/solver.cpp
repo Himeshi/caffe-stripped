e4bade0c1d37f370bc73213852ea24c451ead477
Himeshi <himeshidslv@gmail.com>
2019-07-17 12:18:33 +0800

    Print output values in float

--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -248,12 +248,12 @@ void Solver<Dtype>::Step(int iters) {
         const string& output_name =
             net_->blob_names()[net_->output_blob_indices()[j]];
         const Dtype loss_weight =
-            net_->blob_loss_weights()[net_->output_blob_indices()[j]];
+            fp16tofp32(net_->blob_loss_weights()[net_->output_blob_indices()[j]]);
         for (int k = 0; k < result[j]->count(); ++k) {
           ostringstream loss_msg_stream;
           if (loss_weight) {
             loss_msg_stream << " (* " << loss_weight
-                            << " = " << loss_weight * result_vec[k] << " loss)";
+                            << " = " << loss_weight * fp16tofp32(result_vec[k]) << " loss)";
           }
           LOG_IF(INFO, Caffe::root_solver()) << "    Train net output #"
               << score_index++ << ": " << output_name << " = "
@@ -405,9 +405,9 @@ void Solver<Dtype>::Test(const int test_net_id) {
     const int output_blob_index =
         test_net->output_blob_indices()[test_score_output_id[i]];
     const string& output_name = test_net->blob_names()[output_blob_index];
-    const Dtype loss_weight = test_net->blob_loss_weights()[output_blob_index];
+    const Dtype loss_weight = fp16tofp32(test_net->blob_loss_weights()[output_blob_index]);
     ostringstream loss_msg_stream;
-    const Dtype mean_score = test_score[i] / param_.test_iter(test_net_id);
+    const Dtype mean_score = fp16tofp32(test_score[i]) / param_.test_iter(test_net_id);
     if (loss_weight) {
       loss_msg_stream << " (* " << loss_weight
                       << " = " << loss_weight * mean_score << " loss)";
