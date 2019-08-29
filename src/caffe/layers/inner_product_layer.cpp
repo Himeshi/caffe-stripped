9a28e8b42006c119324b48573f25e7d5858f7fa7
Himeshi <himeshidslv@gmail.com>
2019-08-29 11:55:12 +0800

    Remove creating backup float blobs (since cudnn is not used)

--- src/caffe/layers/inner_product_layer.cpp
+++ src/caffe/layers/inner_product_layer.cpp
@@ -40,7 +40,6 @@ void InnerProductLayer<Dtype>::LayerSetUp(const vector<Blob<fp16>*>& bottom,
       weight_shape[1] = K_;
     }
     this->blobs_[0].reset(new Blob<fp16>(weight_shape));
-    this->blobs_dtype_[0].reset(new Blob<Dtype>(weight_shape));
     // fill the weights
     shared_ptr<Filler<Dtype> > weight_filler(GetFiller<Dtype>(
         this->layer_param_.inner_product_param().weight_filler()));
@@ -49,7 +48,6 @@ void InnerProductLayer<Dtype>::LayerSetUp(const vector<Blob<fp16>*>& bottom,
     if (bias_term_) {
       vector<int> bias_shape(1, N_);
       this->blobs_[1].reset(new Blob<fp16>(bias_shape));
-      this->blobs_dtype_[1].reset(new Blob<Dtype>(bias_shape));
       shared_ptr<Filler<Dtype> > bias_filler(GetFiller<Dtype>(
           this->layer_param_.inner_product_param().bias_filler()));
       bias_filler->Fill(this->blobs_[1].get());
