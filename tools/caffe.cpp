02f16f7d6e1eb6c6085037519b01f8d8bc39df98
Himeshi <himeshidslv@gmail.com>
2019-10-01 00:37:51 +0800

    Edit log message

--- tools/caffe.cpp
+++ tools/caffe.cpp
@@ -455,7 +455,7 @@ int main(int argc, char** argv) {
   // Run tool or show usage.
   caffe::GlobalInit(&argc, &argv);
   // computing with fp16
-  LOG(INFO) << "Using compression with float16";
+  LOG(INFO) << "Using compression with float16,7";
   if (argc == 2) {
 #ifdef WITH_PYTHON_LAYER
     try {
