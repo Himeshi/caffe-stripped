47bfab18082a5df5638650bceaf8b721ce6cbbd8
Himeshi <himeshidslv@gmail.com>
2019-11-05 14:25:51 +0800

    Convert both train and test databases in the same script

--- tools/convert_to_posit_lmdb.cpp
+++ tools/convert_to_posit_lmdb.cpp
@@ -58,9 +58,7 @@ string int_array_to_string(fp16 int_array[], int size_of_array) {
 	return oss.str();
 }
 
-int main(int argc, char** argv) {
-	printf("Converting to posit(%d, %d)\n", _G_NBITS, _G_ESIZE);
-
+void convert_train_db() {
 	scoped_ptr<db::DB> db_read(db::GetDB (FLAGS_backend));
 	db_read->Open("/home/himeshi/dl/caffe-stripped/examples/mnist/mnist_train_lmdb", db::READ);
 	scoped_ptr<db::Cursor> cursor_read(db_read->NewCursor());
@@ -127,6 +125,83 @@ int main(int argc, char** argv) {
 	}
 	txn->Commit();
 	printf("\n%d\n\n", count);
+}
+
+void convert_test_db() {
+	scoped_ptr<db::DB> db_read(db::GetDB (FLAGS_backend));
+	db_read->Open("/home/himeshi/dl/caffe-stripped/examples/mnist/mnist_test_lmdb", db::READ);
+	scoped_ptr<db::Cursor> cursor_read(db_read->NewCursor());
+
+	scoped_ptr<db::DB> db_write(db::GetDB (FLAGS_backend));
+	std::ostringstream s;
+	s << "/home/himeshi/dl/caffe-stripped/examples/mnist/mnist_test_posit_" << _G_NBITS << "_" << _G_ESIZE << "_lmdb";
+	db_write->Open(s.str(), db::NEW);
+	scoped_ptr<db::Transaction> txn(db_write->NewTransaction());
+	scoped_ptr<db::Cursor> cursor_write(db_write->NewCursor());
+
+	Datum datum;
+	DatumNew datumNew;
+	int count = 0;
+	while(cursor_read->valid()) {
+		datum.ParseFromString(cursor_read->value());
+
+		const int datum_channels = datum.channels();
+		const int datum_height = datum.height();
+		const int datum_width = datum.width();
+
+		datumNew.set_channels(datum_channels);
+		datumNew.set_height(datum_height);
+		datumNew.set_width(datum_width);
+		datumNew.set_label(fp32tofp16(datum.label()));
+		//datumNew.set_float_data(datum.float_data());
+		datumNew.set_encoded(datum.encoded());
+
+		int size = datum_channels * datum_width * datum_height;
+		uint16_t* dataNew = (uint16_t*) malloc(size * sizeof(uint16_t));
+		const string& data = datum.data();
+
+		int data_index;
+		float datum_element;
+		for (int c = 0; c < datum_channels; ++c) {
+			for (int h = 0; h < datum_height; ++h) {
+				for (int w = 0; w < datum_width; ++w) {
+					data_index = (c * datum_height + h) * datum_width + w;
+					datum_element = static_cast<float>(static_cast<uint8_t>(data[data_index])) * 0.00390625;
+					dataNew[data_index] = (uint16_t) fp32tofp16(datum_element);
+					/*if(data_index < 784 && datum_element != 0.0)
+						printf("%d %f %hu, ", data_index, datum_element, dataNew[data_index]);*/
+				}
+			}
+		}
+		//printf("\n");
+
+		char* dataNewBytes = (char*) malloc(size * sizeof(uint16_t));
+		for (int j = 0; j < size * 2; j += 2) {
+			dataNewBytes[j] = dataNew[j / 2] >> 8;
+			dataNewBytes[j + 1] = dataNew[j / 2] & 0x00FF;
+		}
+		datumNew.set_data(dataNewBytes, size * 2);
+
+		string output;
+		datumNew.SerializeToString(&output);
+
+		txn->Put(cursor_read->key(), output);
+
+		free(dataNew);
+		free(dataNewBytes);
+		count++;
+		cursor_read->Next();
+	}
+	txn->Commit();
+	printf("\n%d\n\n", count);
+}
+
+int main(int argc, char** argv) {
+	printf("Converting to posit(%d, %d)\n", _G_NBITS, _G_ESIZE);
+
+	convert_train_db();
+
+	convert_test_db();
 
 
 	//Validation
