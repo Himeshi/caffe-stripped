15286cc1ea666d37336f26e3f010cc5b90b639cf
Himeshi <himeshidslv@gmail.com>
2019-12-04 22:31:17 +0800

    Remove unused math function headers

--- include/caffe/util/math_functions.hpp
+++ include/caffe/util/math_functions.hpp
@@ -169,18 +169,6 @@ void caffe_gpu_gemm_half_with_float(const CBLAS_TRANSPOSE TransA,
 	fp16* C);
 
 template <typename Dtype>
-void caffe_gpu_gemm_half_with_floatB(const CBLAS_TRANSPOSE TransA,
-    const CBLAS_TRANSPOSE TransB, const int M, const int N, const int K,
-    const Dtype alpha, const fp16* A, const Dtype* B, const Dtype beta,
-	fp16* C);
-
-template <typename Dtype>
-void caffe_gpu_gemm_half_with_float_inputs(const CBLAS_TRANSPOSE TransA,
-    const CBLAS_TRANSPOSE TransB, const int M, const int N, const int K,
-    const Dtype alpha, const Dtype* A, const Dtype* B, const Dtype beta,
-	fp16* C);
-
-template <typename Dtype>
 void caffe_gpu_gemv(const CBLAS_TRANSPOSE TransA, const int M, const int N,
     const Dtype alpha, const Dtype* A, const Dtype* x, const Dtype beta,
     Dtype* y);
