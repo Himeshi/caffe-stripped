11f902804c7b951a4c778b328a7a0a7cf1dca82b
Himeshi <himeshidslv@gmail.com>
2020-01-03 18:12:28 +0800

    Update log message for dlfloat

--- tools/caffe.cpp
+++ tools/caffe.cpp
@@ -455,7 +455,7 @@ int main(int argc, char** argv) {
   // Run tool or show usage.
   caffe::GlobalInit(&argc, &argv);
   // computing with fp16
-  LOG(INFO) << "Using compression with float16,6";
+  LOG(INFO) << "Using compression with dlfloat";
   if (argc == 2) {
 #ifdef WITH_PYTHON_LAYER
     try {
