a97b96251cf426a70729f432a0c40a9345c3b75c
Himeshi <himeshidslv@gmail.com>
2020-01-30 11:17:04 +0800

    Reduce value sample bins

--- src/caffe/sampling.cpp
+++ src/caffe/sampling.cpp
@@ -29,7 +29,10 @@ void sample_blob(const fp16* blob, int blob_count, std::map<int, int> &exp_map, 
 #endif
 
 #ifdef SAMPLE_VALUES
-		val_map[temp & 0x7FFF]++;
+		if(temp != 0)
+		  val_map[(temp & 0x7FFF) >> 2]++;
+		else
+		  val_map[0]++;
 #endif
 	}
 }
