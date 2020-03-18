caebf81d82852178c17cf344e7d41c5351aa0e68
Himeshi <himeshidslv@gmail.com>
2020-03-18 19:42:58 +0800

    Add custom weight and bias sample frequencies

--- include/caffe/sampling.hpp
+++ include/caffe/sampling.hpp
@@ -34,6 +34,10 @@ union Bits {
 
 #define SAMPLING_FREQ 100
 
+#define WEIGHT_SAMPLING_FREQ 10
+
+#define BIAS_SAMPLING_FREQ 1
+
 namespace caffe {
 
 void sample_blob(const float* blob, int blob_count, std::map<int, int> &exp_map, std::map<int, int> &frac_map, std::map<int, int> &val_map, std::vector<uint32_t> &val_vector, int sampling_frequency);
