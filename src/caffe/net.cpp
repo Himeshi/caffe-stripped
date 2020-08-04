dbb33293d29183c74cfae4f352ed1c4a8d3910cf
Himeshi <himeshidslv@gmail.com>
2020-08-05 01:46:11 +0800

    Create a dtype blob for every fp16 blob

--- src/caffe/net.cpp
+++ src/caffe/net.cpp
@@ -388,28 +388,19 @@ void Net<Dtype>::AppendTop(const NetParameter& param, const int layer_id,
       LOG(INFO) << layer_param->name() << " -> " << blob_name;
     }
     shared_ptr<Blob<fp16> > blob_pointer(new Blob<fp16>());
-    //shared_ptr<Blob<Dtype> > blob_pointer_dtype(new Blob<Dtype>());
+    shared_ptr<Blob<Dtype> > blob_pointer_dtype(new Blob<Dtype>());
     const int blob_id = blobs_.size();
     blobs_.push_back(blob_pointer);
-    //blobs_dtype_.push_back(blob_pointer_dtype);
+    blobs_dtype_.push_back(blob_pointer_dtype);
     blob_names_.push_back(blob_name);
     blob_need_backward_.push_back(false);
     if (blob_name_to_idx) { (*blob_name_to_idx)[blob_name] = blob_id; }
     top_id_vecs_[layer_id].push_back(blob_id);
     top_vecs_[layer_id].push_back(blob_pointer.get());
-    typename vector<shared_ptr<Blob<Dtype> > >::iterator it = blobs_dtype_.begin();
     if(layer_param->type() == "Accuracy") {
         //we want an accuracy layer output blob to be in Dtype to
         //avoid errors due to conversion in the printed result
-        shared_ptr<Blob<Dtype> > blob_pointer_dtype(new Blob<Dtype>());
-        blobs_dtype_.insert(it, blob_id, blob_pointer_dtype);
-        top_vecs_dtype_[layer_id].push_back(blob_pointer_dtype.get());
         net_output_blobs_dtype_.push_back(blob_pointer_dtype.get());
-    }
-    if(layer_param->type() == "Data" || layer_param->type() == "Split") {
-        shared_ptr<Blob<Dtype> > blob_pointer_dtype(new Blob<Dtype>());
-        blobs_dtype_.insert(it, blob_id, blob_pointer_dtype);
-        top_vecs_dtype_[layer_id].push_back(blob_pointer_dtype.get());
     }
   }
   if (available_blobs) { available_blobs->insert(blob_name); }
