649304c5f48e77d0234ffbed3719360e54ad94dc
Himeshi <himeshidslv@gmail.com>
2020-10-07 00:38:53 +0800

    Add bias to relu layer

--- src/caffe/layers/relu_layer.cu
+++ src/caffe/layers/relu_layer.cu
@@ -6,10 +6,10 @@
 namespace caffe {
 
 template <typename Dtype>
-__global__ void ReLUForward(const int n, const fp16* in, fp16* out,
+__global__ void ReLUForward(const int n, const Dtype* in, Dtype* out,
     Dtype negative_slope) {
   CUDA_KERNEL_LOOP(index, n) {
-    out[index] = fp16tofp32_gpu(in[index]) > 0 ? in[index] : fp32tofp16_gpu(fp16tofp32_gpu(in[index]) * negative_slope);
+    out[index] = in[index] > 0 ? in[index] : in[index] * negative_slope;
   }
 }
 
@@ -18,52 +18,62 @@ void ReLULayer<Dtype>::Forward_gpu(const vector<Blob<fp16>*>& bottom,
     const vector<Blob<fp16>*>& top, const vector<Blob<Dtype>*>& bottom_dtype,
     const vector<Blob<Dtype>*>& top_dtype) {
   const fp16* bottom_data = bottom[0]->gpu_data();
-  fp16* top_data = top[0]->mutable_gpu_data();
   const int count = bottom[0]->count();
+  this->temp_bottom_->Reshape(bottom[0]->shape());
+  Dtype* bottom_data_dtype = this->temp_bottom_->mutable_gpu_data();
+  caffe_expand_blob(count, bottom_data_dtype, bottom_data, bottom[0]->data_bias);
+
+  fp16* top_data = top[0]->mutable_gpu_data();
+  this->temp_top_->Reshape(top[0]->shape());
+  Dtype* temp_top_data = this->temp_top_->mutable_gpu_data();
+
   Dtype negative_slope = this->layer_param_.relu_param().negative_slope();
   // NOLINT_NEXT_LINE(whitespace/operators)
   ReLUForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
-      count, bottom_data, top_data, negative_slope);
+      count, bottom_data_dtype, temp_top_data, negative_slope);
+  caffe_compress_blob(top[0]->count(), temp_top_data, top_data, &(top[0]->data_bias));
   CUDA_POST_KERNEL_CHECK;
   // << " count: " << count << " bottom_data: "
   //     << (unsigned long)bottom_data
   //     << " top_data: " << (unsigned long)top_data
   //     << " blocks: " << CAFFE_GET_BLOCKS(count)
   //     << " threads: " << CAFFE_CUDA_NUM_THREADS;
-
-#ifdef SAMPLE_FLOATS
-    if(this->phase_ == TRAIN && this->sample_iter_) {
-      sample_blob(top_data, top[0]->count(), this->activation_exp, this->activation_frac, this->activation, this->activation_vector, SAMPLING_FREQ);
-    }
-#endif
 }
 
 template <typename Dtype>
-__global__ void ReLUBackward(const int n, const fp16* in_diff,
-    const fp16* in_data, fp16* out_diff, Dtype negative_slope) {
+__global__ void ReLUBackward(const int n, const Dtype* in_diff,
+    const Dtype* in_data, Dtype* out_diff, Dtype negative_slope) {
   CUDA_KERNEL_LOOP(index, n) {
-    out_diff[index] = fp32tofp16_gpu(fp16tofp32_gpu(in_diff[index]) * ((fp16tofp32_gpu(in_data[index]) > 0) + (fp16tofp32_gpu(in_data[index]) <= 0) * negative_slope));
+    out_diff[index] = in_diff[index] * ((in_data[index] > 0)
+        + (in_data[index] <= 0) * negative_slope);
   }
 }
 
 template <typename Dtype>
 void ReLULayer<Dtype>::Backward_gpu(const vector<Blob<fp16>*>& top,
     const vector<bool>& propagate_down, const vector<Blob<fp16>*>& bottom,
-	const vector<Blob<Dtype>*>& top_dtype, const vector<Blob<Dtype>*>& bottom_dtype) {
+    const vector<Blob<Dtype>*>& top_dtype, const vector<Blob<Dtype>*>& bottom_dtype) {
   if (propagate_down[0]) {
-    const fp16* bottom_data = bottom[0]->gpu_data();
-    const fp16* top_diff = top[0]->gpu_diff();
-    fp16* bottom_diff = bottom[0]->mutable_gpu_diff();
     const int count = bottom[0]->count();
+    const fp16* bottom_data = bottom[0]->gpu_data();
+    this->temp_bottom_->Reshape(bottom[0]->shape());
+    Dtype* bottom_data_dtype = this->temp_bottom_->mutable_gpu_data();
+    caffe_expand_blob(count, bottom_data_dtype, bottom_data, bottom[0]->data_bias);
+
+    const int top_count = top[0]->count();
+    const fp16* top_diff = top[0]->gpu_diff();
+    this->temp_top_->Reshape(top[0]->shape());
+    Dtype* top_diff_dtype = this->temp_top_->mutable_gpu_diff();
+    caffe_expand_blob(top_count, top_diff_dtype, top_diff, top[0]->diff_bias);
+
+    fp16* bottom_diff = bottom[0]->mutable_gpu_diff();
+    Dtype* bottom_diff_dtype = this->temp_bottom_->mutable_gpu_diff();
+
     Dtype negative_slope = this->layer_param_.relu_param().negative_slope();
     // NOLINT_NEXT_LINE(whitespace/operators)
     ReLUBackward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
-        count, top_diff, bottom_data, bottom_diff, negative_slope);
-#ifdef SAMPLE_FLOATS
-      if(this->phase_ == TRAIN && this->sample_iter_) {
-        sample_blob(bottom[0]->gpu_diff(), bottom[0]->count(), this->activation_gradient_exp, this->activation_gradient_frac, this->activation_gradient, this->activation_gradient_vector, SAMPLING_FREQ);
-      }
-#endif
+        count, top_diff_dtype, bottom_data_dtype, bottom_diff_dtype, negative_slope);
+    caffe_compress_blob(count, bottom_diff_dtype, bottom_diff, &(bottom[0]->diff_bias));
     CUDA_POST_KERNEL_CHECK;
   }
 }
