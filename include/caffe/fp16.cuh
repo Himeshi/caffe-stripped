0493e032115d455966e4722e052031dfefa6ac7a
Himeshi <himeshidslv@gmail.com>
2020-12-02 11:43:52 +0800

    Add posit conversion

--- include/caffe/fp16.cuh
+++ include/caffe/fp16.cuh
@@ -104,5 +104,77 @@ __device__ __inline__ fp16 fp32tofp16_gpu(float f) {
   return p;
 }
 
+__device__ __inline__ float fp16tofp32_gpu_IP(fp16 p) {
+  union Bits v;
+
+  // get sign
+  bool sign = p & SIGN_MASK;
+  p = (p ^ -sign) + sign;
+
+  // get the regime sign
+  bool regime_sign = p & SECOND_BIT_MASK;
+
+  // get regime
+  v.ui = p << POSIT_LENGTH_PLUS_ONE;
+  //int regime_length = (__builtin_clz(v.ui) & -!regime_sign) + (__builtin_clz(~v.ui) & -regime_sign);
+  int regime_length;
+  if(regime_sign)
+    regime_length = (__clz(~v.ui));
+  else
+	regime_length = (__clz(v.ui));
+  int regime = (regime_length - regime_sign) << _G_ESIZE_IP;
+  regime = (regime ^ -regime_sign) + regime_sign;
+
+  // assemble
+  v.ui <<= (regime_length + 1);
+  v.ui >>= (FLOAT_SIGN_PLUS_EXP_LENGTH - _G_ESIZE_IP);
+  v.ui += ((SINGLE_PRECISION_BIAS - regime) << FLOAT_EXPONENT_SHIFT);
+
+  v.si ^= (FLOAT_INF ^ v.si) & -(p == _G_INFP);
+  v.si ^= (0 ^ v.si) & -(p == 0);
+
+  v.ui |= (sign << FLOAT_SIGN_SHIFT);
+  return v.f;
+}
+
+__device__ __inline__ fp16 fp32tofp16_gpu_IP(float f) {
+  fp16 p = 0;
+  union Bits v;
+  v.f = f;
+  bool sign = v.ui & FLOAT_SIGN_MASK;
+  v.ui &= 0x7FFFFFFF;
+
+  p ^= (p ^_G_MAXREALP_IP) & -(v.si >= _G_MAXREAL_INT_IP);
+  p ^= (p ^ _G_INFP) & -(v.si >= FLOAT_INF);
+  p ^= (p ^ _G_MINREALP_IP) & -(v.si != 0 && v.si <= _G_MINREAL_INT_IP);
+
+  // min posit exponent in 16, 3 is 112
+  // therefore all the float subnormals will be handled
+  // in the previous if statement
+
+  // get exponent sign
+  bool exp_sign = !(v.ui >> FLOAT_EXP_SIGN_SHIFT);
+
+  //get regime and exponent
+  uint32_t exp = abs((v.si >> FLOAT_EXPONENT_SHIFT) - SINGLE_PRECISION_BIAS);
+  TEMP_TYPE regime_and_exp = (((1 << ((exp >> _G_ESIZE_IP) + 1)) - 1) << (_G_ESIZE_IP + 1)) | (exp & POSIT_EXPONENT_MASK_IP);;
+  //if exponent is negative
+  regime_and_exp = ((regime_and_exp ^ -exp_sign) + exp_sign) >> ((exp_sign & !((exp & POSIT_EXPONENT_MASK_IP))) & (bool) exp);
+  int regime_and_exp_length = (exp >> _G_ESIZE_IP) + 2 + _G_ESIZE_IP - ((exp_sign & !((exp & POSIT_EXPONENT_MASK_IP))) & (bool) exp);
+
+  //assemble
+  regime_and_exp <<= (UNSIGNED_LONG_LONG_SIZE - regime_and_exp_length);
+  regime_and_exp |= ((TEMP_TYPE) (v.ui & FLOAT_FRACTION_MASK) << (POSIT_EXP_SHIFT - regime_and_exp_length));
+  fp16 temp_p = (regime_and_exp >> POSIT_EXTRA_BITS_SHIFT_IP);
+
+  //round
+  temp_p += (bool) (regime_and_exp & POSIT_HALFWAY_BIT_MASK_IP) && ((temp_p & 1) | (regime_and_exp & POSIT_EXTRA_BITS_MASK_IP));
+  p ^= (temp_p ^ p) & -((v.si < _G_MAXREAL_INT_IP) & (v.si > _G_MINREAL_INT_IP));
+
+  p = (p ^ -sign) + sign;
+
+  return p;
+}
+
 }
 #endif /* INCLUDE_CAFFE_FP16_HPP_ */
