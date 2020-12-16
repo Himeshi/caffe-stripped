c06ce75f6a3ebdf4d8ce976f4c5591715c78ce1c
Himeshi <himeshidslv@gmail.com>
2020-12-16 13:06:40 +0800

    Save blobs with new format

--- src/caffe/blob.cpp
+++ src/caffe/blob.cpp
@@ -869,7 +869,7 @@ void Blob<fp16>::ToProto(BlobProto* proto, bool write_diff) const {
   proto->clear_diff();
   const fp16* data_vec = cpu_data();
   for (int i = 0; i < count_; ++i) {
-    proto->add_data(fp16tofp32(data_vec[i]) * data_bias);
+    proto->add_data(fp16tofp32_W(data_vec[i]) * data_bias);
   }
   if (write_diff) {
     const fp16* diff_vec = cpu_diff();
